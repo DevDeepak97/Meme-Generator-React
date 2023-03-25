0af7a587c9342f7f5abcf998624d8092cdf40c77
DevDeepak97 <deepakyadav17890@gmail.com>
2023-03-25 13:49:13 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+export default function Header() {
+    return (
+        <>
+        <header className="header">
+            <img 
+                src="/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">Deepak</h4>
+        </header>
+        </>
+        
+    )
+}
